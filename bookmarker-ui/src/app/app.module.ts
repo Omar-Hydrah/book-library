9d57358cdc3de3a9c637d129596e62571ceb0da9
Omar Hydrah <omarhydrah@tutanota.de>
2019-05-26 15:09:52 +0200

    Fix imported file name

--- bookmarker-ui/src/app/app.module.ts
+++ bookmarker-ui/src/app/app.module.ts
@@ -7,10 +7,10 @@ import { MainComponent } from "./main/main.component";
 import { ListComponent } from './list/list.component';
 import { BookComponent } from './book/book.component';
 import { BookPageComponent } from './book-page/book-page.component';
-import { FavoriteListComponent } from './favorites-list/favorites-list.component';
+import { FavoriteListComponent } from './favorite-list/favorite-list.component';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 import { BookService } from "./service/book.service";
-import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
     declarations: [
