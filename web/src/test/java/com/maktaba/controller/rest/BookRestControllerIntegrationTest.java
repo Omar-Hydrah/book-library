e24bc8a3959d8e6e3b0024643e9b5c9637f1653d
Omar Hydrah <omarhydrah@tutanota.de>
2019-06-16 18:13:23 +0200

    Update BookRestController tests

--- web/src/test/java/com/maktaba/controller/rest/BookRestControllerIntegrationTest.java
+++ web/src/test/java/com/maktaba/controller/rest/BookRestControllerIntegrationTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -22,6 +23,9 @@ import org.junit.Test;
 import org.junit.Before;
 
 import org.mockito.Mockito;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.any;
 
 import java.util.List;
 import java.util.Arrays;
@@ -29,10 +33,12 @@ import javax.sql.DataSource;
 
 import com.maktaba.service.BookService;
 import com.maktaba.model.Book;
+import com.maktaba.model.Author;
 
 
 /*@TestPropertySource(properties=
 	{"spring.autoconfigure.exclude=com.maktaba.config.SecurityConfig"})*/
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @WebMvcTest(BookRestController.class)
 @AutoConfigureMockMvc(secure=false)
@@ -59,5 +65,41 @@ public class BookRestControllerIntegrationTest {
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$", hasSize(1)))
 			.andExpect(jsonPath("$[0].title", is(book.getTitle())));
+	}
+
+
+	@Test
+	public void whenGetBookById_thenReturnJsonArray() throws Exception{
+		Book book = new Book("Physics", "Introduction to Physics");
+
+		Mockito.when(service.findById(anyLong())).thenReturn(book);
+
+		mvc.perform(
+			get("/api/book/2").contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.title", is(book.getTitle())));
+			// .andExpect(jsonPath("$", hasSize(1)))
+	}
+
+	@Test
+	public void whenPostBook_thenSaveAndReturnBookJson() throws Exception{
+
+
+		Book book = new Book("Physics", "Introduction to Physics");
+		Author author = new Author("Mark Anthony");
+		book.setAuthor(author);
+
+		Mockito.when(service.save(any(Book.class))).thenReturn(book);
+
+		mvc.perform(post("/api/book/create")
+				.param("title", book.getTitle())
+				.param("description", book.getDescription()) 
+				.param("author.name", author.getName())
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.title", is(book.getTitle())))
+			.andExpect(jsonPath("$.author.name", is(author.getName())));
+			// .andExpect(jsonPath("$", hasSize(1)))
+
 	}
 }
