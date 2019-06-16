b54f160151a03c618fea80f321951549223e544c
Omar Hydrah <omarhydrah@tutanota.de>
2019-06-16 16:37:47 +0200

    Update BookService tests

--- web/src/test/java/com/maktaba/service/BookServiceIntegrationTest.java
+++ web/src/test/java/com/maktaba/service/BookServiceIntegrationTest.java
@@ -10,6 +10,8 @@ import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 import org.junit.runner.RunWith;
 import org.junit.Before;
@@ -22,6 +24,8 @@ import java.util.List;
 import java.util.ArrayList;
 
 import org.mockito.Mockito;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.any;
 
 import com.maktaba.model.Book;
 import com.maktaba.model.Author;
@@ -46,23 +50,69 @@ public class BookServiceIntegrationTest{
 	@MockBean
 	private BookRepository bookRepository;
 
+	private Book book1;
+	private Book book2;
+	private List<Book> bookList = new ArrayList<>();
+
 	@Before
 	public void setUp(){
-		Book book = new Book("Physics", "Introduction to Physics");
-		List<Book> bookList = new ArrayList<>(); 
-		bookList.add(book);
+		this.book1 = new Book("Physics", "Introduction to Physics");
+		this.book2 = new Book("Chemistry", "Introduction to Chemistry");
 
-		Mockito.when(bookRepository.findByTitle(book.getTitle()))
-			.thenReturn(bookList);
+		this.bookList.add(book1);
+		this.bookList.add(book2);
+
 	}
 
 	@Test
 	public void whenFindBookByTitle_thenReturnBookList(){
 		Book book = new Book("Physics", "Introduction to Physics");
+
+		// mock
+		Mockito.when(bookRepository.findByTitle(book.getTitle()))
+			.thenReturn(this.bookList);
+
+		// when
 		List<Book> foundList = bookService.findByTitle(book.getTitle());
 
-		logger.info("\n\n" + foundList.toString() + "\n\n" );
 
+		// then
 		assertThat(foundList.get(0).getTitle(), is(book.getTitle()));
 	}
+
+	@Test
+	public void whenFindBookByAuthorId_thenReturnBookList(){
+
+		logger.info("\n\n" + this.bookList.toString() + "\n\n" );
+
+		// mock
+		Mockito.when(bookRepository.findByAuthorId(any(Long.class)))
+			.thenReturn(this.bookList);
+
+		// when 
+		List<Book> foundList = bookService.findByAuthorId(Long.valueOf(1));
+
+		// then
+		assertThat(foundList, hasItems(this.book1, this.book2));
+		assertThat(foundList, hasSize(2));
+
+	}
+
+	@Test
+	public void whenFindAllBooks_thenReturnBookList(){
+
+		logger.info("\n\n" + this.bookList.toString() + "\n\n" );
+
+		// mock
+		Mockito.when(bookRepository.findAll())
+			.thenReturn(this.bookList);
+
+		// when
+		List<Book> foundList = bookService.findAll();
+
+		// then
+		assertThat(foundList, hasItems(this.book1, this.book2));
+		assertThat(foundList, hasSize(2));
+	}
+
 }
