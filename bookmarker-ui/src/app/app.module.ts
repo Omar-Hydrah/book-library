3d0a09f3e120c97cc28787113d121aecc2c926e4
Omar Hydrah <omarhydrah@tutanota.de>
2019-06-23 11:34:28 +0200

    Import services and add BookListComponent

--- bookmarker-ui/src/app/app.module.ts
+++ bookmarker-ui/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from "./main/main.component";
-import { ListComponent } from './list/list.component';
+import { BookListComponent } from './book-list/book-list.component';
 import { BookComponent } from './book/book.component';
 import { BookPageComponent } from './book-page/book-page.component';
 import { FavoriteListComponent } from './favorite-list/favorite-list.component';
@@ -12,6 +14,7 @@ import { FavoriteComponent } from './favorite/favorite.component';
 
 import { BookService } from "./service/book.service";
 import { FavoriteService } from "./service/favorite.service";
+import { AuthorService } from "./service/author.service";
 import { BookFormComponent } from './book-form/book-form.component';
 import { FavoriteFormComponent } from './favorite-form/favorite-form.component';
 import { NavigationComponent } from './navigation/navigation.component';
@@ -20,7 +23,7 @@ import { NavigationComponent } from './navigation/navigation.component';
     declarations: [
         AppComponent,
         MainComponent,
-        ListComponent,
+        BookListComponent,
         BookComponent,
         BookPageComponent,
         FavoriteListComponent,
@@ -31,11 +34,14 @@ import { NavigationComponent } from './navigation/navigation.component';
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule
     ],
     providers: [
         BookService,
         FavoriteService,
+        AuthorService,
     ],
     bootstrap: [AppComponent]
 })
