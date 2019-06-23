bb9eeb8bdfed207c86bce9e2deb4d5fc37d2fa2c
Omar Hydrah <omarhydrah@tutanota.de>
2019-06-23 12:17:16 +0200

    Add routes for Forms

--- bookmarker-ui/src/app/app-routing.module.ts
+++ bookmarker-ui/src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ListComponent } from "./list/list.component";
-import { MainComponent } from "./main/main.component";
-import { BookPageComponent } from "./book-page/book-page.component";
+import { Routes, RouterModule }  from '@angular/router';
+import { MainComponent }         from "./main/main.component";
+import { BookPageComponent }     from "./book-page/book-page.component";
+import { BookListComponent }     from "./book-list/book-list.component";
+import { BookFormComponent }     from "./book-form/book-form.component";
+import { FavoriteFormComponent }     from "./favorite-form/favorite-form.component";
+import { AuthorFormComponent }     from "./author-form/author-form.component";
 
 const routes: Routes = [
-	{path: ":id", component: BookPageComponent},
-	{path: "", component: MainComponent, pathMatch: "full"},
+	{path: ":id",             component: BookPageComponent},
+	{path: "list/:id",        component: BookListComponent},
+	{path: "book/create",     component: BookFormComponent},
+	{path: "favorite/create", component: FavoriteFormComponent},
+	{path: "author/create",   component: AuthorFormComponent},
+	{path: "",                component: MainComponent, pathMatch: "full"},
+
 ];
 
 @NgModule({
