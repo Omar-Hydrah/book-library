2525ae33ee6d4c8291b48635b1c0319b6951d028
Omar Hydrah <omarhydrah@tutanota.de>
2019-06-24 13:02:29 +0200

    Update relation between Author and Book and ignore books field

--- web/src/main/java/com/maktaba/model/Author.java
+++ web/src/main/java/com/maktaba/model/Author.java
@@ -5,12 +5,15 @@ import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.CascadeType;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
+@JsonIgnoreProperties({"books"})
 public class Author{
 
 	@Id
@@ -20,7 +23,7 @@ public class Author{
 	@Column(nullable=false, unique=true)
 	private String name;
 
-	@ManyToMany(cascade=CascadeType.ALL)
+	@OneToMany(cascade=CascadeType.ALL)
 	private List<Book> books;
 
 	public Author(){
