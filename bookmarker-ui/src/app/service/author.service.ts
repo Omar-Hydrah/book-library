524aae3bad10eddcc54eac482ee1f5098844a1cf
Omar Hydrah <omarhydrah@tutanota.de>
2019-06-25 12:46:49 +0200

    Add getFavorite(id) and getAll() to favorite service

--- bookmarker-ui/src/app/service/author.service.ts
+++ bookmarker-ui/src/app/service/author.service.ts
@@ -10,21 +10,31 @@ import { Author } from "../model/";
 export class AuthorService {
     constructor(private http : HttpClient) {}
 
+    getAll() : Observable<Author[]>{
+        return this.http.get<Author[]>("/api/author/all")
+            .pipe(catchError(this.handleError<any>("getAll")));
+    }
+
+    getAuthor(id : number) : Observable<Author>{
+        return this.http.get<Author>("/api/author/find/" + id)
+            .pipe(catchError(this.handleError<any>("getAuthor", id)));
+    }
+
     createAuthor(author : Author) : Observable<Author>{
-    	return this.http.post<Author>("/api/author/create", author)
+    	return this.http.post<Author>("/api/author/create/", author)
     		.pipe(catchError(this.handleError("createAuthor", author)));
     }
 
-    searchByName(authorName : String) : Observable<Author[]>{
-    	return this.http.get<Author[]>("/api/author/" + authorName)
-    		.pipe(catchError(this.handleError("searchByName", authorName)));
+    searchByName(name : string) : Observable<Author[]>{
+    	return this.http.get<Author[]>("/api/author/" + name)
+    		.pipe(catchError(this.handleError<Author[]>("searchByName")));
     		
     }
 
     private handleError<T>(operation="operation", result ? : T){
-    	return (error : any) : Observable<T> =>{
-    		console.error(error);
-    		return of(result as T);
-    	};
+        return (error : any) : Observable<T> =>{
+            console.error(error);
+            return of(result as T);
+        }
     }
 }
