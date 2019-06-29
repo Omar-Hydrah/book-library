b4eda66150ebbdac04bd4c0081740f6358ee68b3
Omar Hydrah <omarhydrah@tutanota.de>
2019-06-29 23:42:17 +0200

    Handle form submission and display a list of the authors in book-form

--- bookmarker-ui/src/app/book-form/book-form.component.ts
+++ bookmarker-ui/src/app/book-form/book-form.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from "@angular/forms";
-import { BookService } from "../service/book.service";
-import { Book } from "../model/";
+import { BookService, AuthorService } from "../service/";
+import { Book, Author } from "../model/";
+
+// declare var $ : any; 
 
 @Component({
     selector: 'book-form-component',
@@ -10,22 +12,51 @@ import { Book } from "../model/";
 })
 export class BookFormComponent implements OnInit {
 
-	bookForm : FormGroup;
-	title    : FormControl;
-	description : FormControl;
+	bookForm         : FormGroup;
+	titleField       : FormControl;
+	descriptionField : FormControl;
+    authorField      : FormControl; 
+    authors          : Author[]; 
+    book             : Book = {
+        id: null,
+        title: "",
+        description: "",
+        author : {id: null, name: ""}
+    }
 
-    constructor(private bookService : BookService) {}
+    constructor(private bookService : BookService, 
+        private authorService : AuthorService) {}
+
     ngOnInit() {
-    	this.title = new FormControl("");
-    	this.description = new FormControl("");
+        this.authorService.getAll().subscribe((authors : Author[])=>{
+            this.authors = authors;
+        });
 
-    	this.bookForm = new FormGroup({
-    		title : this.title,
-    		description : this.description
-    	});
+        this.titleField       = new FormControl("");
+        this.authorField      = new FormControl("");
+        this.descriptionField = new FormControl("");
+
+        this.bookForm = new FormGroup({
+            titleField       : this.titleField,
+            authorField      : this.authorField,
+            descriptionField : this.descriptionField,
+        });
+
     }
 
 	handleSubmit(){
-		
+        this.book.title       = this.titleField.value;
+        this.book.description = this.descriptionField.value;
+        this.book.author = this.authors.filter(author => {
+            return author.id == this.authorField.value
+        })[0];
+        
+        this.bookService.createBook(this.book).subscribe((book : Book)=>{
+            if(book != null){
+                console.log("Book saved");
+            }else{
+                console.log("Failed to save book");
+            }
+        });
 	}    
 }
