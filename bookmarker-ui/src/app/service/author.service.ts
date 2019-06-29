fe52bdd1309ba22258ccd58e00c65a28844f2c64
Omar Hydrah <omarhydrah@tutanota.de>
2019-06-29 23:36:02 +0200

    Send the plain text name in createAuthor() with HttpHeaders

--- bookmarker-ui/src/app/service/author.service.ts
+++ bookmarker-ui/src/app/service/author.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from "rxjs";
 import { catchError } from "rxjs/operators";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Author } from "../model/";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthorService {
+    private httpOptions = {
+        headers : new HttpHeaders({
+            "Content-Type" : "application/json"
+        }),
+    };
+
     constructor(private http : HttpClient) {}
 
     getAll() : Observable<Author[]>{
@@ -20,9 +26,10 @@ export class AuthorService {
             .pipe(catchError(this.handleError<any>("getAuthor", id)));
     }
 
-    createAuthor(author : Author) : Observable<Author>{
-    	return this.http.post<Author>("/api/author/create/", author)
-    		.pipe(catchError(this.handleError("createAuthor", author)));
+    createAuthor(name : string) : Observable<Author>{
+    	return this.http.post<Author>("/api/author/create/", name, 
+            this.httpOptions)
+    		.pipe(catchError(this.handleError<Author>("createAuthor")));
     }
 
     searchByName(name : string) : Observable<Author[]>{
