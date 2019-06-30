a51f7a234daae85128cdaddce2c2a432438cc068
Omar Hydrah <omarhydrah@tutanota.de>
2019-06-30 02:54:48 +0200

    Display book details, and allow adding a book to a favorite

--- bookmarker-ui/src/app/book-page/book-page.component.ts
+++ bookmarker-ui/src/app/book-page/book-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Book } from "../model/index";
+import { ActivatedRoute, Router } from "@angular/router";
+import { FormGroup, FormControl } from "@angular/forms";
+import { Book, Favorite } from "../model/index";
+import { BookService, FavoriteService } from "../service/";
 
 @Component({
     selector: 'book-page-component',
@@ -7,8 +10,50 @@ import { Book } from "../model/index";
     styleUrls: ['./book-page.component.css']
 })
 export class BookPageComponent implements OnInit {
-	@Input() book : Book;
+	private book : Book = {
+		id: null, 
+		title: "Title", 
+		description: "Description",
+		author: {id: null, name: "Author"}
+	};
 
-    constructor() {}
-    ngOnInit() {}
+	private favoriteList : Favorite[];
+	private bookFavoriteForm : FormGroup;
+	private favoriteField    : FormControl;
+	private selectedFavorite : Favorite;
+
+    constructor(private bookService : BookService, 
+    	private favoriteService : FavoriteService, 
+    	private route : ActivatedRoute, private router : Router) {}
+
+    ngOnInit() {
+    	this.favoriteField    = new FormControl("");
+    	this.bookFavoriteForm = new FormGroup({
+    		favoriteField: this.favoriteField,
+    	});
+
+    	this.route.params.subscribe((param : any)=>{
+    		this.bookService.getBook(param["id"]).subscribe((book : Book)=>{
+    			this.book = book;
+    		});
+    	});
+
+    	this.favoriteService.getAll().subscribe((favoriteList : Favorite[])=>{
+    		this.favoriteList = favoriteList;
+    	});
+    }
+
+    handleSubmit(){
+    	var favoriteId = this.favoriteField.value;
+
+    	this.favoriteService.addBookToFavorite(this.book, favoriteId)
+    	.subscribe((favorite : Favorite)=>{
+    		if(favorite != null){
+    			console.log("Added book to favorite");
+    			this.router.navigate([`/favorite/${favoriteId}`]);
+    		}else{
+    			console.log("Failed to add book");
+    		}
+    	})
+    }
 }
