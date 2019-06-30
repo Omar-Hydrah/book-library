15ed7cb7b4226cca365d87b7818c2ae70d0bf0e2
Omar Hydrah <omarhydrah@tutanota.de>
2019-06-30 14:42:31 +0200

    Add validation to AuthorForm

--- bookmarker-ui/src/app/author-form/author-form.component.ts
+++ bookmarker-ui/src/app/author-form/author-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { AuthorService } from "../service/";
 import { Author } from "../model/";
 
@@ -16,7 +16,10 @@ export class AuthorFormComponent implements OnInit {
 
     constructor(private authorService : AuthorService) {}
     ngOnInit() {
-    	this.nameField = new FormControl("");
+    	this.nameField = new FormControl("", [
+            Validators.required,
+            Validators.minLength(4),
+        ]);
     	this.authorForm = new FormGroup({
     		nameField : this.nameField,
     	});
@@ -25,6 +28,10 @@ export class AuthorFormComponent implements OnInit {
     }
 
     handleSubmit(){
+        if(this.nameField.errors){
+            return;
+        }
+
         var name = this.nameField.value;
         this.authorService.createAuthor(name)
         .subscribe((author : Author)=>{
